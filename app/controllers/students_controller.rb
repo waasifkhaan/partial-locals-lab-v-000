c32e354fe270ffbacdd8ec839203b73c448fd897
Wasif Khan <waasif.khaan@gmail.com>
2018-10-14 19:41:39 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -24,6 +24,7 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+
   def student_params
     params.require(:student).permit(:name, :birthday, :hometown)
   end
